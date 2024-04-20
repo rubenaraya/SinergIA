3147c7bd5269833efa38abc1b5d9d0654c53cec5
Ruben Araya Tagle <rubenarayatagle@gmail.com>
2024-04-19 21:31:48 -0400

    actualizacion

--- backend/prueba/base/enrutador.py
+++ backend/prueba/base/enrutador.py
@@ -1,1 +1,19 @@
 # backend\prueba\base\enrutador.py
+
+from fastapi import APIRouter, Request
+from fastapi.responses import JSONResponse
+from starlette.templating import Jinja2Templates
+
+prueba_base = APIRouter()
+
+@prueba_base.get('/{aplicacion}/{servicio}')
+def prueba(aplicacion: str, servicio: str):
+    return {aplicacion: servicio}
+
+@prueba_base.get('/prueba/base/{accion}', response_class=JSONResponse)
+def accion(request:Request, accion: str):
+    """ cómo pasar parámetros a la plantilla """
+    ruta_plantillas = '/prueba/base/'
+    plantillas = Jinja2Templates(directory = f"./backend")
+    respuesta = plantillas.TemplateResponse(f".{ruta_plantillas}plantilla_basica.json", context={"request":request})
+    return respuesta
--- backend/pysinergia/interfaz.py
+++ backend/pysinergia/interfaz.py
@@ -1,1 +1,32 @@
 # backend\pysinergia\interfaz.py
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+class Aplicacion():
+    def crear(dir_static:str) -> FastAPI:
+        app = FastAPI()
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins = ['*'],
+            allow_credentials = True,
+            allow_methods = ["*"],
+            allow_headers = ["*"],
+        )
+        app.mount(
+            f"/{dir_static}",
+            StaticFiles(directory = "./frontend"),
+            name = "static"
+        )
+
+        # variables env a app.config
+        # logger
+        # excepciones
+
+        # Endpoints de la Aplicación
+        @app.get('/')
+        def home():
+            return {'inicio': 'funcionando'}
+
+        return app
