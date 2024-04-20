ee78c5deff2d83de6696337da64d8a91df1cb12b
Ruben Araya Tagle <rubenarayatagle@gmail.com>
2024-04-20 17:55:56 -0400

    actualizacion

--- backend/pysinergia/interfaz.py
+++ backend/pysinergia/interfaz.py
@@ -3,16 +3,18 @@
 from fastapi import FastAPI
 import os
 
+# --------------------------------------------------
+# Clase: ServidorApi
+# --------------------------------------------------
 class ServidorApi():
 
-    def __init__(mi, modo:str='PRODUCCION', registro:str='ERROR'):
-        mi.modo = modo
-        mi.registro = registro
-        mi.prefijo = ''
+# --------------------------------------------------
+# Métodos privados
+# --------------------------------------------------
 
     def _configurar_endpoints(mi, api:FastAPI):
         @api.get('/')
-        def home():
+        def entrypoint():
             return {'entrypoint-api': 'SinergIA'}
 
     def _configurar_cors(mi, api:FastAPI):
@@ -27,53 +29,32 @@ class ServidorApi():
 
     def _configurar_directorio(mi, api:FastAPI, directorio:str, alias:str):
         from fastapi.staticfiles import StaticFiles
-        api.mount(
-            f"/{alias}",
-            StaticFiles(directory = f"{directorio}"),
-            name = "static"
-        )
+        api.mount(f"/{alias}", StaticFiles(directory=f"{directorio}"), name="static")
 
-    def _configurar_entorno(mi, api:FastAPI):
-        from dotenv import dotenv_values
-        archivos = {
-            'DESARROLLO': ".desarrollo.env",
-            'PRUEBAS': ".pruebas.env",
-            'PRODUCCION': ".produccion.env",
-            'LOCAL': ".local.env"
-        }
-        ruta_archivo = os.path.join(os.path.abspath("."), archivos.get(mi.modo))
-        if os.path.isfile(ruta_archivo):
-            claves = dotenv_values(ruta_archivo)
-            for clave, valor in claves.items():
-                # print(f"{clave}={valor}")
-                ...
+# --------------------------------------------------
+# Métodos públicos
+# --------------------------------------------------
 
-    def crear_api(mi, directorio:str, alias:str, prefijo:str='') -> FastAPI:
-        mi.prefijo = prefijo
+    def crear_api(mi, directorio:str, alias:str) -> FastAPI:
         api = FastAPI()
         mi._configurar_directorio(api, directorio, alias)
         mi._configurar_endpoints(api)
         mi._configurar_cors(api)
         return api
 
-    def mapear_servicios(mi, api:FastAPI, ubicacion:str):
+    def mapear_enrutadores(mi, api:FastAPI, ubicacion:str):
         import importlib
-        directorios = os.listdir(ubicacion)
-        for directorio in directorios:
-            if directorio != 'pysinergia':
-                subdirectorios = os.listdir(f"{ubicacion}/{directorio}")
-                for subdirectorio in subdirectorios:
-                    ruta_archivo = os.path.join(ubicacion, directorio, subdirectorio, 'enrutador.py')
+        aplicaciones = os.listdir(ubicacion)
+        for aplicacion in aplicaciones:
+            if aplicacion != 'pysinergia':
+                servicios = os.listdir(f"{ubicacion}/{aplicacion}")
+                for servicio in servicios:
+                    ruta_archivo = os.path.join(ubicacion, aplicacion, servicio, 'enrutador.py')
                     if os.path.isfile(ruta_archivo):
-                        componente = importlib.import_module(f"{ubicacion}.{directorio}.{subdirectorio}.enrutador")
-                        api.include_router(getattr(componente, 'enrutador'))
+                        enrutador = importlib.import_module(f"{ubicacion}.{aplicacion}.{servicio}.enrutador")
+                        api.include_router(getattr(enrutador, 'enrutador'))
 
-    def lanzar(mi, api:FastAPI, host:str, puerto:int):
+    def iniciar_servicio(mi, api:FastAPI, host:str, puerto:int, modo:str):
         import uvicorn
-        if mi.modo == 'LOCAL' or mi.modo == 'DESARROLLO':
-            uvicorn.run(api, 
-                host = host,
-                port = puerto, 
-                ssl_keyfile = "./key.pem", 
-                ssl_certfile = "./cert.pem"
-            )
+        if modo == 'LOCAL' or modo == 'DESARROLLO':
+            uvicorn.run(api, host=host, port=puerto, ssl_keyfile="./key.pem", ssl_certfile="./cert.pem")
