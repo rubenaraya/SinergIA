091bb95e9d1a63ce4e002ed5ddf277fc3f5ce54b
Ruben Araya Tagle <rubenarayatagle@gmail.com>
2024-06-03 21:48:50 -0400

    actualizacion

--- pysinergia/exportadores/convertidor_audio.py
+++ pysinergia/exportadores/convertidor_audio.py
@@ -1,18 +1,21 @@
 # pysinergia\exportadores\convertidor_audio.py
 
-from pathlib import Path
 import math, os
+from pathlib import Path
+
 # --------------------------------------------------
 # Importaciones de PySinergIA
-from pysinergia import (
-    Constantes as _Constantes,
-    ErrorPersonalizado as _ErrorPersonalizado,
-)
+from pysinergia import Constantes as _Constantes
 from pysinergia.adaptadores import I_ConectorDisco as _I_Disco
 
+# --------------------------------------------------
+# Configuracion de FFMPEG
 RUTA_FFMPEG = Path(f'{_Constantes.DIR_LIB_FFMPEG}').resolve()
 os.environ["PATH"] = str(RUTA_FFMPEG) + os.pathsep + os.getenv("PATH")
 from pydub import AudioSegment
+
+# --------------------------------------------------
+# Clases de Constantes
 
 class FORMATO:
     WAV = 'wav'
@@ -47,15 +50,14 @@ class ConvertidorAudio:
             'bitrate': BITRATE._64KBPS,
         }
 
-    # --------------------------------------------------
-    # Métodos públicos
-
     def convertir(mi, ruta_audio:str, dir_destino:str, opciones:dict=None) -> list[str]:
         try:
             if not opciones:
                 opciones = mi.opciones
+
             ruta_destino = mi.disco.crear_carpeta(nombre=dir_destino, antecesores=True)
             ruta_origen = mi.disco.comprobar_ruta(ruta_audio)
+
             resultado:list = []
             if ruta_origen and ruta_destino and opciones:
                 ruta_origen_path = Path(ruta_origen)
@@ -65,23 +67,24 @@ class ConvertidorAudio:
                 tiempo_maximo = opciones.get('tiempo_maximo')
                 bitrate = opciones.get('bitrate')
                 canales = opciones.get('canales')
-                audio = AudioSegment.from_file(ruta_origen)
+                audio:AudioSegment = AudioSegment.from_file(ruta_origen)
+                if canales == 1:
+                    audio = audio.split_to_mono()[0]
                 audio = audio.set_channels(canales)
-                duracion = round(audio.duration_seconds, None) + 1
-                cantidad = math.ceil(duracion * 1000 / tiempo_maximo)
+                cantidad = math.ceil(len(audio) / tiempo_maximo)
                 for i in range(0, cantidad):
                     inicio = (i * tiempo_maximo)
                     final = inicio + (tiempo_maximo)
                     seleccion = audio[inicio:final]
                     if cantidad > 1:
-                        ruta_final = f"{ruta_destino}/{nombre_base}-{str(i + 1)}.{formato}"
+                        ruta_guardar = f"{ruta_destino}/{nombre_base}-{str(i + 1)}.{formato}"
                     else:
-                        ruta_final = f"{ruta_destino}/{nombre_base}.{formato}"
-                    seleccion.export(ruta_final, format=formato, bitrate=bitrate)
-                    if Path(ruta_final).stat().st_size > peso_maximo:
+                        ruta_guardar = f"{ruta_destino}/{nombre_base}.{formato}"
+                    seleccion.export(ruta_guardar, format=formato, bitrate=bitrate)
+                    if Path(ruta_guardar).stat().st_size > peso_maximo:
                         break
                     else:
-                        resultado.append(ruta_final)
+                        resultado.append(ruta_guardar)
             return resultado
         except Exception as e:
             print(e)
